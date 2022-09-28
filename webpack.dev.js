626a6fdfe90968d069e7b3d327891f1daff504dc
Sooji Kim <yh0gogo@gmail.com>
2022-09-28 16:14:50 +0900

    🔨add more bundling settings

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,15 @@
 'use strict';
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval',
   devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
     historyApiFallback: true,
     port: 3000,
     hot: true,
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,22 @@
 'use strict';
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+      new CssMinimizerPlugin(),
+    ],
+  },
   //   devtool: "hidden-source-map",
 });
