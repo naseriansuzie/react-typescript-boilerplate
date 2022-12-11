4326d63ba715b850b76dbb5f160eab12e4027915
Sooji Kim <yh0gogo@gmail.com>
2022-12-11 19:32:54 +0900

    💄 add styled component example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import GlobalStyle from '@styles/globalStyle';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  background: #000000;
+  color: gold;
+`;
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div>Hello :)</div>
+      <Title>Hello :)</Title>
     </>
   );
 }
